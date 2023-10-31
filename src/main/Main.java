c32a9b786a3a3b96c236e1cc88f09c2971b26b08
AusaSwart <october.monroe@mail.ru>
2023-10-31 18:01:35 +0300

    find.smallest.integer mine version

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,41 +2,50 @@ package main;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(SmashWords.smash("Smash", "Words"));
-
+        System.out.print(" Args 1, 2, -15, 29. Smallest one is ");
+        System.out.println(SmallestIntegerFinder.findSmallestInt(new int[]{1, 2, -15, 29}));
     }
 
-    public static class SmashWords {
-        public static String smash (String ... words){
-            return Stream.of(words).collect(Collectors.joining(" "));
-            //  stream.collect() method is used to perform a mutable reduction operation
-            //  on the elements of a stream. It returns a new mutable object containing
-            //  the results of the reduction operation.
+    public static class SmallestIntegerFinder {
+        public static int findSmallestInt(int[] args) {
+
+            int smallest = args[0];
+            for (Integer arg: args) {
+                if ( smallest > arg )
+                    smallest = arg;
+            }
+            return smallest;
         }
     }
 
-    public static class SmashWordsTest {
+    public static class SmallestIntegerFinderTest {
+
         @Test
-        public void validate(){
-            assertEquals("Smash Words", SmashWords.smash("Smash", "Words"));
+        public void example1(){
+            int expected = 11;
+            int actual = SmallestIntegerFinder.findSmallestInt(new int[]{78,56,232,12,11,43});
+            assertEquals(expected, actual);
+        }
+
+
+        @Test
+        public void example2(){
+            int expected = -33;
+            int actual = SmallestIntegerFinder.findSmallestInt(new int[]{78,56,-2,12,8,-33});
+            assertEquals(expected, actual);
         }
 
         @Test
-        public void validateEmpty() {
-            assertEquals("", SmashWords.smash());
-        }
-
-        @Test
-        public void validateOneWord(){
-            assertEquals("Smash", SmashWords.smash("Smash"));
+        public void example3(){
+            int expected = Integer.MIN_VALUE;
+            int actual = SmallestIntegerFinder.findSmallestInt(new int[]{0,Integer.MIN_VALUE,Integer.MAX_VALUE});
+            assertEquals(expected, actual);
         }
     }
 
